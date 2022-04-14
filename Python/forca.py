0c60033f05e069dc26ec3a79d4ba3182ecdb478a
William Pereira dos Santos <william.sanctos@gmail.com>
2022-04-14 00:01:55 -0300

    25% Curso alura 2

--- Python/forca.py
+++ Python/forca.py
@@ -1,7 +1,24 @@
 def jogar_forca():
     print("*********************************")
-    print("Bem vindo ao jogo de Forca!")
+    print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 1
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando...")
 
     print("Fim de jogo!")
 
